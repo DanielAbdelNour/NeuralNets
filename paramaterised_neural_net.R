503f68baa28ac3bf226d5376eddeffc8c6a88f99
Daniel N <daniel.a.nour@gmail.com>
2016-08-09 20:05:47 +0930

    inbuit auto synapse, layer, and delta computation

--- paramaterised_neural_net.R
+++ paramaterised_neural_net.R
@@ -1,6 +1,12 @@
-###    Dan net V1     ###
+###    Dan Net V1     ###
 # By Daniel Abdel-Nour #
 ########################
+
+# TEMP VARS #
+hidden.layers <- c(3,2)
+x <- mtcars$mpg/max(mtcars$mpg)
+x <- matrix(runif(10,-1,1),nrow = 5, ncol =2)
+# # # # # # 
 
 dan.net <- function(x, y, hidden.layers = c(3,2), cost.function = "sse", alpha = 0.1, max.iter = 100000){
     x <- as.matrix(x)
@@ -21,10 +27,15 @@ dan.net <- function(x, y, hidden.layers = c(3,2), cost.function = "sse", alpha =
         return((y_hat - y)/(y_hat*(1 - y_hat)))
     }
     
-    # parameterise hidden layers
+    # set var for number of hidden layers
     n.hl <- length(hidden.layers)
     
-    # init weights
+    # init delta list for the derivatives of each layer
+    delta <- list()
+    
+    # init weights and layers
+    l <- list()
+    l[[1]] <- x
     wts <- list()
     wt0 <- matrix(runif(dim(x)[2] * hidden.layers[1]), nrow = dim(x)[2], ncol = hidden.layers[1])
     wts[[1]] <- wt0
@@ -37,4 +48,46 @@ dan.net <- function(x, y, hidden.layers = c(3,2), cost.function = "sse", alpha =
         
         wts[[(p+1)]] <- wtn
     }
+    
+    # start training 
+    for(i in c(1:max.iter)){
+      
+      # FORWARD PASS #
+      # apply the activiation function to the dot product of layers and corrosponding weights
+      for(q in c(1:(n.hl + 1))){
+        l[[(q + 1)]] <- sig(l[[q]] %*% wts[[q]])
+      }
+      
+      # BACKPROPAGATION #
+      
+      for(r in c((n.hl + 1):1)){
+        # compute the derivative of the loss function at the output later
+        if(r == (n.hl + 1))
+          delta[[r]] <- cross.entropy.prime(l[[r]]) * sig.prime(l[[r]])
+        else
+          delta[[r]] <- (delta[[(r + 1)]] %*% t(wts[[r]])) * sig.prime(l[[r]])
+      }
+      
+      l3_delta <- entropy.prime(l3) * sig.prime(l3)
+      l2_delta <- (l3_delta %*% t(syn2)) * sig.prime(l2)
+      l1_delta <- (l2_delta %*% t(syn1)) * sig.prime(l1)
+      
+      syn2 <- syn2 - (alpha * t(l2) %*% l3_delta)
+      syn1 <- syn1 - (alpha * t(l1) %*% l2_delta)
+      syn0 <- syn0 - (alpha * t(x) %*% l1_delta)
+      
+      if(i %% 1000 == 0){
+        print(paste(abs(sum(y - l2)), " : ", (i/1000000) * 100))
+      }
+    }
 }
+
+
+
+
+
+
+
+
+
+
